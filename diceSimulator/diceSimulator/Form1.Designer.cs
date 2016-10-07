9cfbd6042386bd928d0e24a0c12a5da89469ee4f
Jenkintr <Jenkintr@users.noreply.github.com>
2016-10-06 23:59:56 -0500

    Add files via upload

---
+++ diceSimulator/diceSimulator/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace diceSimulator
+{
+    partial class dieSimulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(dieSimulator));
+            this.dice1PictureBox = new System.Windows.Forms.PictureBox();
+            this.dice2PictureBox = new System.Windows.Forms.PictureBox();
+            this.rollButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.dice6PictureBox = new System.Windows.Forms.PictureBox();
+            this.dice5PictureBox = new System.Windows.Forms.PictureBox();
+            this.dice4PictureBox = new System.Windows.Forms.PictureBox();
+            this.dice3PictureBox = new System.Windows.Forms.PictureBox();
+            this.dice6PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dice5PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dice4PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dice3PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dice2PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dice1PictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice6PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice6PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dice1PictureBox
+            // 
+            this.dice1PictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice1PictureBox.BackgroundImage")));
+            this.dice1PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice1PictureBox.Location = new System.Drawing.Point(65, 8);
+            this.dice1PictureBox.Name = "dice1PictureBox";
+            this.dice1PictureBox.Size = new System.Drawing.Size(105, 105);
+            this.dice1PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice1PictureBox.TabIndex = 0;
+            this.dice1PictureBox.TabStop = false;
+            // 
+            // dice2PictureBox
+            // 
+            this.dice2PictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice2PictureBox.BackgroundImage")));
+            this.dice2PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice2PictureBox.Location = new System.Drawing.Point(65, 8);
+            this.dice2PictureBox.Name = "dice2PictureBox";
+            this.dice2PictureBox.Size = new System.Drawing.Size(105, 105);
+            this.dice2PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice2PictureBox.TabIndex = 1;
+            this.dice2PictureBox.TabStop = false;
+            this.dice2PictureBox.Visible = false;
+            // 
+            // rollButton
+            // 
+            this.rollButton.Location = new System.Drawing.Point(95, 127);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(75, 23);
+            this.rollButton.TabIndex = 2;
+            this.rollButton.Text = "Roll";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(186, 127);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // dice6PictureBox
+            // 
+            this.dice6PictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice6PictureBox.BackgroundImage")));
+            this.dice6PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice6PictureBox.Location = new System.Drawing.Point(65, 8);
+            this.dice6PictureBox.Name = "dice6PictureBox";
+            this.dice6PictureBox.Size = new System.Drawing.Size(105, 105);
+            this.dice6PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice6PictureBox.TabIndex = 4;
+            this.dice6PictureBox.TabStop = false;
+            this.dice6PictureBox.Visible = false;
+            // 
+            // dice5PictureBox
+            // 
+            this.dice5PictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice5PictureBox.BackgroundImage")));
+            this.dice5PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice5PictureBox.Location = new System.Drawing.Point(65, 8);
+            this.dice5PictureBox.Name = "dice5PictureBox";
+            this.dice5PictureBox.Size = new System.Drawing.Size(105, 105);
+            this.dice5PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice5PictureBox.TabIndex = 5;
+            this.dice5PictureBox.TabStop = false;
+            this.dice5PictureBox.Visible = false;
+            // 
+            // dice4PictureBox
+            // 
+            this.dice4PictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice4PictureBox.BackgroundImage")));
+            this.dice4PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice4PictureBox.Location = new System.Drawing.Point(65, 8);
+            this.dice4PictureBox.Name = "dice4PictureBox";
+            this.dice4PictureBox.Size = new System.Drawing.Size(105, 105);
+            this.dice4PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice4PictureBox.TabIndex = 6;
+            this.dice4PictureBox.TabStop = false;
+            this.dice4PictureBox.Visible = false;
+            // 
+            // dice3PictureBox
+            // 
+            this.dice3PictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice3PictureBox.BackgroundImage")));
+            this.dice3PictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice3PictureBox.Location = new System.Drawing.Point(65, 8);
+            this.dice3PictureBox.Name = "dice3PictureBox";
+            this.dice3PictureBox.Size = new System.Drawing.Size(105, 105);
+            this.dice3PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice3PictureBox.TabIndex = 7;
+            this.dice3PictureBox.TabStop = false;
+            this.dice3PictureBox.Visible = false;
+            // 
+            // dice6PictureBox2
+            // 
+            this.dice6PictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice6PictureBox2.BackgroundImage")));
+            this.dice6PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice6PictureBox2.Location = new System.Drawing.Point(186, 8);
+            this.dice6PictureBox2.Name = "dice6PictureBox2";
+            this.dice6PictureBox2.Size = new System.Drawing.Size(105, 105);
+            this.dice6PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice6PictureBox2.TabIndex = 8;
+            this.dice6PictureBox2.TabStop = false;
+            this.dice6PictureBox2.Visible = false;
+            // 
+            // dice5PictureBox2
+            // 
+            this.dice5PictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice5PictureBox2.BackgroundImage")));
+            this.dice5PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice5PictureBox2.Location = new System.Drawing.Point(186, 8);
+            this.dice5PictureBox2.Name = "dice5PictureBox2";
+            this.dice5PictureBox2.Size = new System.Drawing.Size(105, 105);
+            this.dice5PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice5PictureBox2.TabIndex = 9;
+            this.dice5PictureBox2.TabStop = false;
+            this.dice5PictureBox2.Visible = false;
+            // 
+            // dice4PictureBox2
+            // 
+            this.dice4PictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice4PictureBox2.BackgroundImage")));
+            this.dice4PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice4PictureBox2.Location = new System.Drawing.Point(186, 8);
+            this.dice4PictureBox2.Name = "dice4PictureBox2";
+            this.dice4PictureBox2.Size = new System.Drawing.Size(105, 105);
+            this.dice4PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice4PictureBox2.TabIndex = 10;
+            this.dice4PictureBox2.TabStop = false;
+            this.dice4PictureBox2.Visible = false;
+            // 
+            // dice3PictureBox2
+            // 
+            this.dice3PictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice3PictureBox2.BackgroundImage")));
+            this.dice3PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice3PictureBox2.Location = new System.Drawing.Point(186, 8);
+            this.dice3PictureBox2.Name = "dice3PictureBox2";
+            this.dice3PictureBox2.Size = new System.Drawing.Size(105, 105);
+            this.dice3PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice3PictureBox2.TabIndex = 11;
+            this.dice3PictureBox2.TabStop = false;
+            this.dice3PictureBox2.Visible = false;
+            // 
+            // dice2PictureBox2
+            // 
+            this.dice2PictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice2PictureBox2.BackgroundImage")));
+            this.dice2PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice2PictureBox2.Location = new System.Drawing.Point(186, 8);
+            this.dice2PictureBox2.Name = "dice2PictureBox2";
+            this.dice2PictureBox2.Size = new System.Drawing.Size(105, 105);
+            this.dice2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice2PictureBox2.TabIndex = 12;
+            this.dice2PictureBox2.TabStop = false;
+            this.dice2PictureBox2.Visible = false;
+            // 
+            // dice1PictureBox2
+            // 
+            this.dice1PictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("dice1PictureBox2.BackgroundImage")));
+            this.dice1PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.dice1PictureBox2.Location = new System.Drawing.Point(186, 8);
+            this.dice1PictureBox2.Name = "dice1PictureBox2";
+            this.dice1PictureBox2.Size = new System.Drawing.Size(105, 105);
+            this.dice1PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice1PictureBox2.TabIndex = 13;
+            this.dice1PictureBox2.TabStop = false;
+            // 
+            // dieSimulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(359, 160);
+            this.Controls.Add(this.dice1PictureBox);
+            this.Controls.Add(this.dice1PictureBox2);
+            this.Controls.Add(this.dice2PictureBox2);
+            this.Controls.Add(this.dice3PictureBox2);
+            this.Controls.Add(this.dice4PictureBox2);
+            this.Controls.Add(this.dice5PictureBox2);
+            this.Controls.Add(this.dice6PictureBox2);
+            this.Controls.Add(this.dice3PictureBox);
+            this.Controls.Add(this.dice4PictureBox);
+            this.Controls.Add(this.dice5PictureBox);
+            this.Controls.Add(this.dice6PictureBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.rollButton);
+            this.Controls.Add(this.dice2PictureBox);
+            this.Name = "dieSimulator";
+            this.Text = "Dice Simulator";
+            ((System.ComponentModel.ISupportInitialize)(this.dice1PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice6PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice6PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice5PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice4PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice3PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox dice1PictureBox;
+        private System.Windows.Forms.PictureBox dice2PictureBox;
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox dice6PictureBox;
+        private System.Windows.Forms.PictureBox dice5PictureBox;
+        private System.Windows.Forms.PictureBox dice4PictureBox;
+        private System.Windows.Forms.PictureBox dice3PictureBox;
+        private System.Windows.Forms.PictureBox dice6PictureBox2;
+        private System.Windows.Forms.PictureBox dice5PictureBox2;
+        private System.Windows.Forms.PictureBox dice4PictureBox2;
+        private System.Windows.Forms.PictureBox dice3PictureBox2;
+        private System.Windows.Forms.PictureBox dice2PictureBox2;
+        private System.Windows.Forms.PictureBox dice1PictureBox2;
+    }
+}
+
---
+++ diceSimulator/diceSimulator/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diceSimulator
+{
+    public partial class dieSimulator : Form
+    {
+        public dieSimulator()
+        {
+            InitializeComponent();
+        }
+
+        private void rollButton_Click(object sender, EventArgs e)
+        {
+            //Variable to indicate which die is shown
+            int dieShown1;
+            int dieShown2;
+
+            //Create Random object.
+            Random rand = new Random();
+
+            //Get a random interger in the range of 0-6 to correspond with the die number
+            dieShown1 = rand.Next(1, 6+1);
+            dieShown2 = rand.Next(1, 6 + 1);
+
+            //Display Dice 1 side that is up
+            if (dieShown1 == 1)
+            {
+                //Display Dice 1 with 1:
+                dice1PictureBox.Visible = true;
+                dice2PictureBox.Visible = false;
+                dice3PictureBox.Visible = false;
+                dice4PictureBox.Visible = false;
+                dice5PictureBox.Visible = false;
+                dice6PictureBox.Visible = false;
+
+                
+            }
+            else if (dieShown1 == 2)
+            {
+                //Display Dice 1 with 2:
+                dice1PictureBox.Visible = false;
+                dice2PictureBox.Visible = true;
+                dice3PictureBox.Visible = false;
+                dice4PictureBox.Visible = false;
+                dice5PictureBox.Visible = false;
+                dice6PictureBox.Visible = false;
+            }
+            else if (dieShown1 == 3)
+            {
+                //Display Dice 1 with 3:
+                dice1PictureBox.Visible = false;
+                dice2PictureBox.Visible = false;
+                dice3PictureBox.Visible = true;
+                dice4PictureBox.Visible = false;
+                dice5PictureBox.Visible = false;
+                dice6PictureBox.Visible = false;
+            }
+            else if (dieShown1 == 4)
+            {
+                //Display Dice 1 with 4:
+                dice1PictureBox.Visible = false;
+                dice2PictureBox.Visible = false;
+                dice3PictureBox.Visible = false;
+                dice4PictureBox.Visible = true;
+                dice5PictureBox.Visible = false;
+                dice6PictureBox.Visible = false;
+            }
+            else if (dieShown1 == 5)
+            {
+                //Display Dice 1 with 5:
+                dice1PictureBox.Visible = false;
+                dice2PictureBox.Visible = false;
+                dice3PictureBox.Visible = false;
+                dice4PictureBox.Visible = false;
+                dice5PictureBox.Visible = true;
+                dice6PictureBox.Visible = false;
+            }
+            else if (dieShown1 == 6)
+            {
+                //Display Dice 1 with 6:
+                dice1PictureBox.Visible = false;
+                dice2PictureBox.Visible = false;
+                dice3PictureBox.Visible = false;
+                dice4PictureBox.Visible = false;
+                dice5PictureBox.Visible = false;
+                dice6PictureBox.Visible = true;
+            }
+
+            
+
+            //Display Dice 2 side that is up
+            if (dieShown2 == 1)
+            {
+                //Display Dice 2 with 1:
+                dice1PictureBox2.Visible = true;
+                dice2PictureBox2.Visible = false;
+                dice3PictureBox2.Visible = false;
+                dice4PictureBox2.Visible = false;
+                dice5PictureBox2.Visible = false;
+                dice6PictureBox2.Visible = false;
+            }
+            else if (dieShown2 == 2)
+            {
+                //Display Dice 2 with 2:
+                dice1PictureBox2.Visible = false;
+                dice2PictureBox2.Visible = true;
+                dice3PictureBox2.Visible = false;
+                dice4PictureBox2.Visible = false;
+                dice5PictureBox2.Visible = false;
+                dice6PictureBox2.Visible = false;
+            }
+            else if (dieShown2 == 3)
+            {
+                //Display Dice 2 with 3:
+                dice1PictureBox2.Visible = false;
+                dice2PictureBox2.Visible = false;
+                dice3PictureBox2.Visible = true;
+                dice4PictureBox2.Visible = false;
+                dice5PictureBox2.Visible = false;
+                dice6PictureBox2.Visible = false;
+            }
+            else if (dieShown2 == 4)
+            {
+                //Display Dice 2 with 4:
+                dice1PictureBox2.Visible = false;
+                dice2PictureBox2.Visible = false;
+                dice3PictureBox2.Visible = false;
+                dice4PictureBox2.Visible = true;
+                dice5PictureBox2.Visible = false;
+                dice6PictureBox2.Visible = false;
+            }
+            else if (dieShown2 == 5)
+            {
+                //Display Dice 2 with 5:
+                dice1PictureBox2.Visible = false;
+                dice2PictureBox2.Visible = false;
+                dice3PictureBox2.Visible = false;
+                dice4PictureBox2.Visible = false;
+                dice5PictureBox2.Visible = true;
+                dice6PictureBox2.Visible = false;
+            }
+            else if (dieShown2 == 6)
+            {
+                //Display Dice 2 with 6:
+                dice1PictureBox2.Visible = false;
+                dice2PictureBox2.Visible = false;
+                dice3PictureBox2.Visible = false;
+                dice4PictureBox2.Visible = false;
+                dice5PictureBox2.Visible = false;
+                dice6PictureBox2.Visible = true;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form.
+            this.Close();
+        }
+    }
+}
